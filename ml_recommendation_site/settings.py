80411d9a21383f006e65f92937be9b259e72354d
Ray Chung <ray.j.chung@gmail.com>
2019-11-09 00:34:04 -0800

    Refer to existing db name.

--- ml_recommendation_site/settings.py
+++ ml_recommendation_site/settings.py
@@ -82,7 +82,7 @@ if 'RDS_HOSTNAME' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['RDS_DB_NAME'],
+            'NAME': os.environ['INTERNAL_DB_NAME'],
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
             'HOST': os.environ['RDS_HOSTNAME'],
